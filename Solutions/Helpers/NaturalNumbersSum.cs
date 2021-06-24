a3324d50eee38f62832c14c268f470e9172a3408
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-06-24 00:43:58 -0300

    First commit, implemented BenchmarkDotNet and added problems 1 and 2.

---
+++ Solutions/Helpers/NaturalNumbersSum.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectEuler
+{
+    public static partial class Helpers
+    {
+        public static ulong NaturalNumbersSum(ulong number)
+        {
+            return number * (number + 1) >> 1;
+        }
+    }
+}
---
+++ Solutions/Solutions.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ProjectEuler
+{
+    [MemoryDiagnoser]
+    public class Solutions
+    {
+        [Arguments(3, 5, 999), Benchmark]
+        public ulong Problem1(ulong p1, ulong p2, ulong p3)
+        {
+            ulong result, product = p1 * p2;
+
+            result = p1 * Helpers.NaturalNumbersSum(p3 / p1)
+                     + p2 * Helpers.NaturalNumbersSum(p3 / p2) 
+                     - product * Helpers.NaturalNumbersSum(p3 / product);
+
+            return result;
+        }
+
+        [Arguments(4000000), Benchmark]
+        public ulong Problem2(ulong p1)
+        {
+            ulong result = 0, before = 1, current = 2, next;
+
+            while (current < p1)
+            {
+                result += current;
+
+                next = before + (current << 1);
+
+                current = next + before + current;
+
+                before = next;
+            }
+
+            return result;
+        }
+    }
+}
